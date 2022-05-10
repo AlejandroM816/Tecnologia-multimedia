b2d1a68d455c61419921d825e15f86652baf617a
Alejandro Muñoz <80214030+AlejandroM816@users.noreply.github.com>
2022-05-11 00:06:21 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -126,7 +126,13 @@ function ModificarContenido(){
         var elementoClickado = e.target.parentNode;
         if(elementoClickado.className.includes('info-item')){
             var nombreAutor= elementoClickado.childNodes[1].alt;
+            console.log(elementoClickado);
+            console.log(elementoClickado.parentNode);
+            console.log(elementoClickado.parentNode.parentNode);
+            console.log(elementoClickado.parentNode.parentNode.parentNode);
+            console.log(elementoClickado.parentNode.parentNode.parentNode.parentNode);
             var subcategoria=elementoClickado.parentNode.parentNode.parentNode.parentNode.childNodes[1].textContent;
+
             var subcategoriajson;
             for(let i=0;i<objeto.subcategory.length;i++){
                     if(objeto.subcategory[i].name.equals(subcategoria)){
