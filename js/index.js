587ae7fd92edab11a1512af7093dac5a0f525287
Alejandro Muñoz <80214030+AlejandroM816@users.noreply.github.com>
2022-05-11 01:41:17 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -149,6 +149,7 @@ function ModificarContenido(){
 
                         var mostrar = document.querySelector('.informacion');
                         var imagen= mostrar.childNodes[3];
+                        console.log("background-image: url(" +subcategoriajson.authors[i].image.name + ")");
                         imagen.style="background-image: url(" +subcategoriajson.authors[i].image.name + ")";
                         $(mostrar).show();
                         i=subcategoriajson.authors.length;
