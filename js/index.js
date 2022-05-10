45f9b92c056491e7a70ebdac690797afed6122ed
Alejandro Muñoz <80214030+AlejandroM816@users.noreply.github.com>
2022-05-10 10:59:06 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ request.onload = function () {
 function SetHeader(objeto) {
     var elemento = document.querySelector(".navbar-nav");
     for (let i = 0; i < objeto.subcategory.length; i++) {
-        var elem='<li class="nav-item"><a class="nav-link" onclick="MoverA(this)" href="#"></a></li>'
+        var elem=document.creaateElement('<li class="nav-item"><a class="nav-link" onclick="MoverA(this)" href="#"></a></li>')
         elem.textContent=objeto.subcategory[i].name;
         elemento.appendChild(elem);
     }
